9c9ec8cade36a3fc7674b3e03dea15293418a55a
julio_firmo <julio_firmo@10.1.48.197>
2013-07-31 16:23:23 -0300

    Trans

---
+++ Revisao/src/br/senai/sc/model/negocio/Transportadora.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.model.negocio;
+
+/**Transportadora
+ *
+ * @author julio_firmo
+ */
+public class Transportadora extends PessoaJuridica {
+    private String tipoTransporte;
+    private double precoFrete;
+    
+     
+    
+    
+}
