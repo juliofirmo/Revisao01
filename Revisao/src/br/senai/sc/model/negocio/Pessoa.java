bd6eb1dc3ac391242b13f13daba5044f028eecdf
julio_firmo <julio_firmo@10.1.48.197>
2013-07-31 14:49:48 -0300

    Adicionando a classe Pessoa

---
+++ Revisao/src/br/senai/sc/model/negocio/Pessoa.java
@@ -0,0 +1,47 @@
+package br.senai.sc.model.negocio;
+
+/**essa classe representa as pessoas do software
+ *
+ * @author julio_firmo
+ */
+public class Pessoa {
+    private int id;
+    private String endereco;
+    private String telefone;
+    private String dataCadastro;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+    
+    
+    
+}
