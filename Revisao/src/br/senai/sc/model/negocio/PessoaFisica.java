4424888ae73d152144bae76196b1087b76ba3ebd
julio_firmo <julio_firmo@10.1.48.197>
2013-07-31 15:01:31 -0300

    Adicionando a classe Pessoa Física

---
+++ Revisao/src/br/senai/sc/model/negocio/PessoaFisica.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.model.negocio;
+
+/**Classe que representa todas as pessoas físicas do software
+ *
+ * @author julio_firmo
+ */
+public class PessoaFisica extends Pessoa {
+    private String cpf;
+    private String nome;
+    private String dataNascimento;
+    private String rg;
+
+    public PessoaFisica() {
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the dataNascimento
+     */
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    /**
+     * @param dataNascimento the dataNascimento to set
+     */
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    /**
+     * @return the rg
+     */
+    public String getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+    
+    
+}
