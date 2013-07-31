1b16e80b539f389f8852c57842cef2440387cf1e
julio_firmo <julio_firmo@10.1.48.197>
2013-07-31 16:15:08 -0300

    Forn

---
+++ Revisao/src/br/senai/sc/model/negocio/Fornecedor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.model.negocio;
+
+/**Classe que representam os fornecedores do software
+ *
+ * @author julio_firmo
+ */
+public class Fornecedor extends PessoaJuridica {
+    
+    private String tipoProduto;
+    
+    
+    
+}
